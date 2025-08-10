8b0e5dc5aec9a0cc493adedcdc97d9e89c9cbaa9
Maria Sidorova <sidmashav@icloud.com>
2025-08-10 16:59:28 +0300

    Added links

--- portfolio/src/components/Project.tsx
+++ portfolio/src/components/Project.tsx
@@ -1,53 +1,57 @@
 import ProjectPhotoGallery from "./ProjectPhotoGallery";
 import { motion } from "motion/react";
 interface ProjectProps {
-	project: {
-		project: string;
-		images: { src: string; alt: string; device: string }[];
-		description: string;
-		tags: string[];
-		link: string;
-		github: string;
-		inProgress?: boolean;
-	};
-	clasName?: string;
+  project: {
+    project: string;
+    images: { src: string; alt: string; device: string }[];
+    description: string;
+    tags: string[];
+    link: string;
+    github: string;
+    inProgress?: boolean;
+  };
+  clasName?: string;
 }
 const Project = ({ project }: ProjectProps) => {
-	return (
-		<motion.div
-			initial={{ opacity: 0, y: 20 }}
-			whileInView={{ opacity: 1, y: 0 }}
-			viewport={{ amount: 0.5, once: true }}
-			transition={{ type: "spring", duration: 2.5 }}
-			className=" flex flex-col gap-5 "
-		>
-			<ProjectPhotoGallery images={project.images} />
-			<div className="flex w-full flex-col gap-1">
-				<h2 className="text-2xl font-semibold text-center">{project.project}</h2>
-				<p className="text-center text-secondary/70 text-sm italic tracking-wide ">
-					{project.description}
-				</p>
-				{project.inProgress && (
-					<p className="text-center text-red-500! text-xs italic tracking-wide">
-						<span className="font-bold">In Progress</span> - this project is still in
-						progress to be better.
-					</p>
-				)}
-			</div>
-			<div className="flex justify-end items-center gap-2 mr-12">
-				{project.tags.map((tag, index) => (
-					<motion.span
-						whileHover={{ scale: 1.1 }}
-						whileTap={{ scale: 0.9 }}
-						key={index}
-						className="text-xs font-semibold text-coffe-light cursor-pointer bg-coffee-brown/20 rounded-full px-2 py-1 hover:bg-coffe-light hover:text-secondary transition-all duration-300 ease-linear"
-					>
-						{tag}
-					</motion.span>
-				))}
-			</div>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ amount: 0.5, once: true }}
+      transition={{ type: "spring", duration: 2.5 }}
+      className=" flex flex-col gap-5 "
+    >
+      <ProjectPhotoGallery images={project.images} />
+      <div className="flex w-full flex-col gap-1">
+        <a href={project.link} target="_blank">
+          <h2 className="text-2xl font-semibold text-center">
+            {project.project}
+          </h2>
+        </a>
+        <p className="text-center text-secondary/70 text-sm italic tracking-wide ">
+          {project.description}
+        </p>
+        {project.inProgress && (
+          <p className="text-center text-red-500! text-xs italic tracking-wide">
+            <span className="font-bold">In Progress</span> - this project is
+            still in progress to be better.
+          </p>
+        )}
+      </div>
+      <div className="flex justify-end items-center gap-2 mr-12">
+        {project.tags.map((tag, index) => (
+          <motion.span
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            key={index}
+            className="text-xs font-semibold text-coffe-light cursor-pointer bg-coffee-brown/20 rounded-full px-2 py-1 hover:bg-coffe-light hover:text-secondary transition-all duration-300 ease-linear"
+          >
+            {tag}
+          </motion.span>
+        ))}
+      </div>
+    </motion.div>
+  );
 };
 
 export default Project;
--- portfolio/src/constants/index.ts
+++ portfolio/src/constants/index.ts
@@ -1,163 +1,163 @@
 import { Coffee, PenTool, NotebookPen, Lightbulb, Code } from "lucide-react";
 export const heroTags = [
-	{ text: "Sketches", icon: NotebookPen },
-	{ text: "Ideas", icon: Lightbulb },
-	{ text: "Code", icon: Code },
-	{ text: "Design", icon: PenTool },
-	{ text: "Coffee?", icon: Coffee },
+  { text: "Sketches", icon: NotebookPen },
+  { text: "Ideas", icon: Lightbulb },
+  { text: "Code", icon: Code },
+  { text: "Design", icon: PenTool },
+  { text: "Coffee?", icon: Coffee },
 ];
 
 export const projects = [
-	{
-		project: "KanBan Board - Visual Task Manager for Projects.",
-		images: [
-			{
-				src: "/images/Project-1/Project-1-dark-main-desktop.png",
-				alt: "Project 1 Dark Main Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-1/Project-1-light-main-desktop.png",
-				alt: "Project 1 Light Main Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-1/Project-1-dark-overview-desktop.png",
-				alt: "Project 1 Dark Overview Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-1/Project-1-light-add-desktop.png",
-				alt: "Project 1 Light Add Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-1/Project-1-dark-main-mobile.png",
-				alt: "Project 1 Dark Main Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-1/Project-1-dark-edit-mobile.png",
-				alt: "Project 1 Dark Edit Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-1/Project-1-light-main-mobile.png",
-				alt: "Project 1 Light Main Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-1/Project-1-light-add-mobile.png",
-				alt: "Project 1 Light Add Mobile",
-				device: "mobile",
-			},
-		],
-		description:
-			"A Frontend Mentor challenge implemented using React, TailwindCSS, Redux, and Motion for a seamless user experience.",
-		tags: ["Vte", "React", "Redux", "TailwindCSS", "Motion"],
-		link: "https://portfolio-flax-pi-68.vercel.app",
-		github: "",
-	},
-	{
-		project: "Notes  - A Simple Note Taking Web App.",
-		images: [
-			{
-				src: "/images/Project-2/Project-2-dark-main-desktop.png",
-				alt: "Project 2 Dark Main Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-2/Project-2-light-login-desktop.png",
-				alt: "Project 2 Light Login Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-2/Project-2-dark-settings-desktop.png",
-				alt: "Project 2 Dark Settings Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-2/Project-2-light-main-desktop.png",
-				alt: "Project 2 Light Main Desktop",
-				device: "desktop",
-			},
+  {
+    project: "KanBan Board - Visual Task Manager for Projects.",
+    images: [
+      {
+        src: "/images/Project-1/Project-1-dark-main-desktop.png",
+        alt: "Project 1 Dark Main Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-1/Project-1-light-main-desktop.png",
+        alt: "Project 1 Light Main Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-1/Project-1-dark-overview-desktop.png",
+        alt: "Project 1 Dark Overview Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-1/Project-1-light-add-desktop.png",
+        alt: "Project 1 Light Add Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-1/Project-1-dark-main-mobile.png",
+        alt: "Project 1 Dark Main Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-1/Project-1-dark-edit-mobile.png",
+        alt: "Project 1 Dark Edit Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-1/Project-1-light-main-mobile.png",
+        alt: "Project 1 Light Main Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-1/Project-1-light-add-mobile.png",
+        alt: "Project 1 Light Add Mobile",
+        device: "mobile",
+      },
+    ],
+    description:
+      "A Frontend Mentor challenge implemented using React, TailwindCSS, Redux, and Motion for a seamless user experience.",
+    tags: ["Vte", "React", "Redux", "TailwindCSS", "Motion"],
+    link: "https://portfolio-flax-pi-68.vercel.app",
+    github: "",
+  },
+  {
+    project: "Notes  - A Simple Note Taking Web App.",
+    images: [
+      {
+        src: "/images/Project-2/Project-2-dark-main-desktop.png",
+        alt: "Project 2 Dark Main Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-2/Project-2-light-login-desktop.png",
+        alt: "Project 2 Light Login Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-2/Project-2-dark-settings-desktop.png",
+        alt: "Project 2 Dark Settings Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-2/Project-2-light-main-desktop.png",
+        alt: "Project 2 Light Main Desktop",
+        device: "desktop",
+      },
 
-			{
-				src: "/images/Project-2/Project-2-dark-all-mobile.png",
-				alt: "Project 2 Dark All Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-2/Project-2-light-add-mobile.png",
-				alt: "Project 2 Dark Add Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-2/Project-2-dark-tag-mobile.png",
-				alt: "Project 2 Dark Tag Mobile",
-				device: "mobile",
-			},
-			{
-				src: "c/images/Project-2/Project-2-light-settings-mobile.png",
-				alt: "Project 2 Light Settings Mobile",
-				device: "mobile",
-			},
-		],
-		description:
-			"A simple note-taking web app built with React, TailwindCSS, and Redux for state management amd local storage as a database.",
-		tags: ["React", "Redux", "TailwindCSS", "Local Storage", "Motion"],
-		link: "https://notes-delta-blue.vercel.app/",
-		github: "",
-	},
-	{
-		project: "Budgets  - A Simple Budget Tracker.",
-		inProgress: true,
-		images: [
-			{
-				src: "/images/Project-3/Project-3-light-overview-desktop.png",
-				alt: "Project 2 Light Overview Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-3/Preojct-3-dark-budgets-desktop.png",
-				alt: "Project 3 Dark Budgets Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-3/Project-3-dark-addP-desktop.png",
-				alt: "Project 3 Dark Add Pot Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-3/Project-3-light-addT-desktop.png",
-				alt: "Project 3 Light Add Transaction Desktop",
-				device: "desktop",
-			},
-			{
-				src: "/images/Project-3/Project-3-dark-overview-mobile.png",
-				alt: "Project 3 Dark Overview Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-3/Project-3-light-t-mobile.png",
-				alt: "Project 3 Light Transactions  Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-3/Project-3-dark-pots-mobile.png",
-				alt: "Project 3 Dark Pots Mobile",
-				device: "mobile",
-			},
-			{
-				src: "/images/Project-3/Project-3-light-recbills-mobile.png",
-				alt: "Project 3 Light Recurring Bills Mobile",
-				device: "mobile",
-			},
-		],
-		description:
-			"A simple budget tracker web app built with React, TailwindCSS, and Redux for state management.",
-		tags: ["Vite", "React", "Redux", "TailwindCSS", "Motion"],
-		link: "https://notes-delta-blue.vercel.app/",
-		github: "",
-	},
+      {
+        src: "/images/Project-2/Project-2-dark-all-mobile.png",
+        alt: "Project 2 Dark All Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-2/Project-2-light-add-mobile.png",
+        alt: "Project 2 Dark Add Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-2/Project-2-dark-tag-mobile.png",
+        alt: "Project 2 Dark Tag Mobile",
+        device: "mobile",
+      },
+      {
+        src: "c/images/Project-2/Project-2-light-settings-mobile.png",
+        alt: "Project 2 Light Settings Mobile",
+        device: "mobile",
+      },
+    ],
+    description:
+      "A simple note-taking web app built with React, TailwindCSS, and Redux for state management amd local storage as a database.",
+    tags: ["React", "Redux", "TailwindCSS", "Local Storage", "Motion"],
+    link: "https://notes-delta-blue.vercel.app/",
+    github: "",
+  },
+  {
+    project: "Budgets  - A Simple Budget Tracker.",
+    inProgress: true,
+    images: [
+      {
+        src: "/images/Project-3/Project-3-light-overview-desktop.png",
+        alt: "Project 2 Light Overview Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-3/Preojct-3-dark-budgets-desktop.png",
+        alt: "Project 3 Dark Budgets Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-3/Project-3-dark-addP-desktop.png",
+        alt: "Project 3 Dark Add Pot Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-3/Project-3-light-addT-desktop.png",
+        alt: "Project 3 Light Add Transaction Desktop",
+        device: "desktop",
+      },
+      {
+        src: "/images/Project-3/Project-3-dark-overview-mobile.png",
+        alt: "Project 3 Dark Overview Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-3/Project-3-light-t-mobile.png",
+        alt: "Project 3 Light Transactions  Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-3/Project-3-dark-pots-mobile.png",
+        alt: "Project 3 Dark Pots Mobile",
+        device: "mobile",
+      },
+      {
+        src: "/images/Project-3/Project-3-light-recbills-mobile.png",
+        alt: "Project 3 Light Recurring Bills Mobile",
+        device: "mobile",
+      },
+    ],
+    description:
+      "A simple budget tracker web app built with React, TailwindCSS, and Redux for state management.",
+    tags: ["Vite", "React", "Redux", "TailwindCSS", "Motion"],
+    link: "https://budget-tracker-flame-eight.vercel.app/",
+    github: "",
+  },
 ];
